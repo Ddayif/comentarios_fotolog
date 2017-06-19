9a46b5c7e723de79e925fbcd84f0aa370c0a3d91
Joselin Grez <joselingrez.m@hotmail.es>
2017-06-19 00:13:32 -0300

    Creando js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,36 @@
 /*aquí va tu código*/
+
+
+	localStorage.nombre = document.getElementById("clave").value;
+	localStorage.comentario = document.getElementById("valor").value;
+
+	localStorage.setItem(clave,valor);
+
+		for(var i = 0; i < localStorage.length; i++){
+		var name = localStorage.key(i);
+		var coment= localStorage.getItem(name);
+	}
+
+	var button = document.getElementById("boton");
+
+	boton.addEventListener("click", function(){
+		var resultado = document.createElement("div");
+
+		var resultado1 = document.createElement("p");
+		var resultado2 = document.createElement("p");
+
+		var parrafoNombre = document.createTextNode(name);
+		var parrafoComentario = document.createTextNode(coment);
+
+		resultado1.appendChild(parrafoNombre);
+		resultado2.appendChild(parrafoComentario);
+
+		resultado.appendChild(resultado1);
+		resultado.appendChild(resultado2);
+	});
+
+function limpiarDatos(){
+	document.getElementById("clear").innerHTML = "Limpio"
+}
+
+
